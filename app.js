c83d9f168352f3b489f816489c9789627aca097e
mohamedderbali <mohamed.derbali@esprit.tn>
2023-02-17 16:40:19 +0100

    db connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 const http = require("http");
 require("dotenv").config();
 
@@ -9,7 +10,15 @@ var indexRouter = require("./routes/index");
 const osRouter = require("./routes/os");
 const productsRouter = require("./routes/products");
 var app = express();
-
+//partie connection sur le DB
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=>{
+  console.log("connected to DB");
+}).catch((err)=>{
+  console.log(err.message)
+})
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
